a534c344775d192349a0d98f3d6f64299bbd28c5
peterpham1102 <youngbuffalo1102@gmail.com>
2020-12-01 15:25:23 +0700

    283 - 298

--- src/store/reducers/result.js
+++ src/store/reducers/result.js
@@ -1,28 +1,26 @@
-import * as actionTypes from '../actions';
+import * as actionTypes from '../actions/actionTypes';
+import { updateObject } from '../utility';
 
 const initialState = {
     results: []
-}
+};
+
+const deleteResult = (state, action) => {
+
+    const updatedArray = state.results.filter(result => result.id !== action.resultElId);
+    return updateObject(state, { results: updatedArray });
+};
 
 const reducer = (state = initialState, action) => {
     switch (action.type) {
-        case actionTypes.STORE_RESULT:
-            return {
-                ...state,
-                results: state.results.concat({id: new Date(), value: action.result})
-            }
-        case actionTypes.DELETE_RESULT:
-            // const id = 2;
-            // const newArray = [...state.results];
-            // newArray.splice(id, 1);
+        // can change data here
+        case actionTypes.STORE_RESULT: return updateObject(state, { results: state.results.concat({ id: new Date(), value: action.result }) });
 
-            const updatedArray = state.results.filter(result => result.id !== action.resultElId );
+        case actionTypes.DELETE_RESULT: return deleteResult(state, action);
+        // const id = 2;
+        // const newArray = [...state.results];
+        // newArray.splice(id, 1);
 
-            return {
-                ...state,
-                results: updatedArray
-
-            }
         default:
             break;
     }
